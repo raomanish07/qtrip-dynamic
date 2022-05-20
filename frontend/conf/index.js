d9a2d1c3976463bc9cb756111cb53d2dbe2fbc1a
CrioUser <crio-user@criodo.com>
2022-05-20 07:50:53 +0530

    “ME_QTRIPDYNAMIC_MODULE_CITIES”

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,5 @@
 
-const config = { backendEndpoint: "http://65.0.40.89:8082" };
+const config = { backendEndpoint: "http://43.204.46.100:8082" };
+
 
 export default config;
--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -1,19 +1,30 @@
 import config from "../conf/index.js";
 
 async function init() {
+  console.log("from init")
   //Fetches list of all cities along with their images and description
   let cities = await fetchCities();
+  console.log(cities)
 
   //Updates the DOM with the cities
   cities.forEach((key) => {
     addCityToDOM(key.id, key.city, key.description, key.image);
   });
+  
 }
 
 //Implementation of fetch call
 async function fetchCities() {
-  // TODO: MODULE_CITIES
-  // 1. Fetch cities using the Backend API and return the data
+    // TODO: MODULE_CITIES
+  // 1. Fetch cities using the Backend API and return the data  
+  try {
+    const response = await fetch(config.backendEndpoint+'/cities');
+    const cities = await response.json();
+    return cities;
+  } catch (error) {
+    console.log('Fetch error: ', error);
+    return null;
+  }
 
 }
 
@@ -21,6 +32,24 @@ async function fetchCities() {
 function addCityToDOM(id, city, description, image) {
   // TODO: MODULE_CITIES
   // 1. Populate the City details and insert those details into the DOM
+  let containerDiv=document.createElement('div');
+  containerDiv.className ="col-6 col-sm-6 col-lg-3 mb-4";
+  let innerHtmlElm=`<a href="pages/adventures/?city=${id}" id="${id}">
+                      <div class="tile">
+                        <img src="${image}"/>
+                        <div class="tile-text text-center">
+                          <h5>${city}</h5>
+                          <p>${description}</p>
+                        </div>
+                      </div>
+                    </a>`
+
+  containerDiv.innerHTML=innerHtmlElm;
+  document.getElementById('data').appendChild(containerDiv)
+  
+
+
+
 
 }
 
